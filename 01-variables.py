360f7dac9dcb0946fd9641abf5a67e1b2509dfcb
ZeRoxyz002 <angelpunonancogm@gmail.com>
2026-05-12 13:50:46 -0400

    formas de imprimir texto

---
+++ 01-variables.py
@@ -0,0 +1,20 @@
+# Definicon de Variables
+nombre = "Angel"
+apellido = "Punonanco"
+edad = 18
+#Lo que se ve en la salida
+
+
+#Forma 1: Clásica separando variables y texto por comas
+print("Mi nombre es", nombre, ", mi apellido es", apellido, "y mi edad es de" , edad, "años")
+
+#Forma 2: Utilizando f-strings
+print(f"Mi nombre es {nombre}, mi apellido es {apellido} y mi edad es de {edad} años")
+
+#Forma 3: Concatenacion (utilizando el operardor +)
+#str(stings es igual a las cadenas de PseInt), se utiliza transforma el valor edad de entero a texto
+print("Mi nombre es " + nombre + " y mi apellido es " + apellido + " y mi edad es de " + str(edad) + "años")
+
+#Utilizando el método input y creando una variable creando una carrera
+carrera = input("¿Que carrera estudias?")
+print(f"Yo estudio la carrera de: {carrera}")
