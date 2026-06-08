cb763f28fdd5599a3b75dbd07d60b4cb1ffc3080
ZeRoxyz002 <angelpunonancogm@gmail.com>
2026-06-08 13:05:46 -0400

    diccionarios

---
+++ 06-diccionarios.py
@@ -0,0 +1,12 @@
+#DICCIONARIOS
+
+paciente= {
+    "nombre":"Benjamin Bahamonde",
+    "edad":18,
+    "ciudad":"Ancud",
+    "fechas_atencion": [5,8,12],
+    "diagnostico": "refrío común"
+}
+
+print(type(paciente))
+print(f"===ficha paciente=== \n{paciente}")
