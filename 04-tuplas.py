7a944de23360c044da46216468fd1c9c61210eeb
ZeRoxyz002 <angelpunonancogm@gmail.com>
2026-06-08 13:01:11 -0400

    tuplas

---
+++ 04-tuplas.py
@@ -0,0 +1,24 @@
+estudiantes= ("Matias", "Fransisco", "Alan", "Maykol")
+print(type(estudiantes))
+
+datos = ([1,2,3,4,5], ("Queilen", "Castro"), ("Universidad de los Lagos", "AIEP"))
+
+print(datos[0])
+print(f"Tuplas: {datos}")
+
+lista_asignaturas = ["Programacion", "Quimica", "Introduccion a la Matematicas"]
+print(f"LISTA: {lista_asignaturas}")
+
+lista_asignaturas.pop()
+print(f"LISTA CON ULTIMO ELEMENENTO BORRADO: {lista_asignaturas}")
+
+
+
+
+print(estudiantes.index("Alan"))
+
+#metodo sorted() para ordenar elementos de una TUPLA
+print(sorted(estudiantes))
+
+
+
